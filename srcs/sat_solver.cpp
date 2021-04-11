df0d634588ec50093a681d28bed4a373655c1b8c
saturn-Tu <38523858+saturn-Tu@users.noreply.github.com>
2021-04-11 15:57:52 +0800

    add: implement vars_columns
    vars_columns is to record the clauses where a specific variable exist

--- srcs/sat_solver.cpp
+++ srcs/sat_solver.cpp
@@ -2,6 +2,14 @@
 #include "sat_solver.h"
 using namespace std;
 
-void sat_solver::parse_file(const char *DIMACS_cnf_file) {
+void sat_solver::init_clauses(const char *DIMACS_cnf_file) {
   parse_DIMACS_CNF(clauses, maxVarIndex, DIMACS_cnf_file);
+  // construct vars_columns
+  vars_columns.resize(maxVarIndex+1);
+  for(int c=0; c<clauses.size(); c++) {
+    for(int v=0; v<clauses[c].size(); v++) {
+      int v_idx = abs(clauses[c][v]);
+      vars_columns[v_idx].insert(c);
+    }
+  }
 }
--- srcs/sat_solver.h
+++ srcs/sat_solver.h
@@ -1,9 +1,15 @@
 #include "parser.h"
+#include <set>
+#include <cstdlib>
 
 class sat_solver {
 public:
   vector< vector<int> > clauses;
   int maxVarIndex;
+  // clause indexs where specific variable exist, <var_idx, clauses_idx>
+  vector< std::set<int> > vars_columns;
 
-  void parse_file(const char *DIMACS_cnf_file);
+  //-------------- function -----------------
+  void init_clauses(const char *DIMACS_cnf_file);
+  void DPLL();
 };
