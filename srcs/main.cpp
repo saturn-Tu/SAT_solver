298cebb76caf4f6da2d9ef1387d759641d283013
saturn-Tu <38523858+saturn-Tu@users.noreply.github.com>
2021-04-11 15:56:02 +0800

    add: Create main.cpp

---
+++ srcs/main.cpp
@@ -0,0 +1,13 @@
+#include "sat_solver.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+  if(argc != 2) {
+    cout << "wrong input format!\n";
+    cout << "./yasat [input.cnf]\n";
+  }
+  sat_solver solver;
+  solver.init_clauses(argv[1]);
+  return 0;
+}
